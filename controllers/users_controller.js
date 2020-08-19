e969b5ab2d334fb607b89face16e1edc70f36889
Ravi Shankar Singh <ravi13eng@gmail.com>
2020-08-19 10:00:23 +0530

    create create session and show profile page done

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,9 +1,22 @@
 const User = require('../models/user');
 
 module.exports.profile = function(req,res){
-    return res.render('user_profile',{
-        title: 'User Profile'
-    })
+    if (req.cookies.user_id){
+        User.findById(req.cookies.user_id, function(err, user){
+            if (user){
+                return res.render('user_profile', {
+                    title: "User Profile",
+                    user: user
+                })
+            }else{
+                return res.redirect('/users/sign-in');
+
+            }
+        });
+    }else{
+        return res.redirect('/users/sign-in');
+
+    }
 }
 
 
@@ -44,3 +57,28 @@ module.exports.create = function(req, res){
     });
 }
 
+//sign in and create a session for the user
+module.exports.createSession = function(req,res){
+    // steps to authenticate
+    // find the user
+    User.findOne({email: req.body.email},function(err,user){
+        if(err){console.log('error in finding user in signing in');return;}
+
+        //handle user found
+        if(user){
+            // handle password whicn doesnot match
+            if(user.password != req.body.password){
+                return res.redirect('back');
+            }
+            res.cookie('user_id', user.id);
+            return res.redirect('/users/profile');
+                
+            }else{
+                // handle user not found
+
+                return res.redirect('back');
+            }
+
+        
+    });
+}
