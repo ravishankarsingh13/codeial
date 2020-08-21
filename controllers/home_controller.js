3fb094f52b031702bdf5fbd249130b2a3063db85
Ravi Shankar Singh <ravi13eng@gmail.com>
2020-08-21 22:28:21 +0530

    showing all user done

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,4 +1,6 @@
 const Post = require('../models/post');
+const User = require('../models/user');
+
 
 module.exports.home = function(req,res){
     // console.log(req.cookies);
@@ -22,9 +24,12 @@ module.exports.home = function(req,res){
         }
     })
     .exec(function(err, posts){
-        return res.render('home', {
-            title: "Codeial | Home",
-            posts:  posts
+        User.find({}, function(err, users){
+            return res.render('home', {
+                title: "Codeial | Home",
+                posts:  posts,
+                all_users: users
+            });
         });
     })
 }
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,9 +1,13 @@
 const User = require('../models/user');
 
-module.exports.profile = function(req,res){
-    return res.render('user_profile',{
-        title: 'User Profile'
-    })
+module.exports.profile = function(req, res){
+    User.findById(req.params.id, function(err, user){
+        return res.render('user_profile', {
+            title: 'User Profile',
+            profile_user: user
+        });
+    });
+
 }
 
 
