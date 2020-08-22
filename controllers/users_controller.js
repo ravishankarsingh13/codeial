cd9f18a7bc0a8f7869521d00233fb5e4cd22c5da
Ravi Shankar Singh <ravi13eng@gmail.com>
2020-08-22 21:09:22 +0530

    noty and flash message done

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -68,11 +68,13 @@ module.exports.create = function(req, res){
 
 // sign in and create a session for the user
 module.exports.createSession = function(req, res){
+    req.flash('success', 'Logged in successfully');
     return res.redirect('/');
 }
 
 module.exports.destroySession = function(req, res){
     req.logout();
+    req.flash('success', 'Logged out');
 
     return res.redirect('/');
 }
