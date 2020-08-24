402779c9f2980703f4bfbcf5db60db1a4d127678
Ravi Shankar Singh <ravi13eng@gmail.com>
2020-08-24 12:34:49 +0530

    ajax and noty done

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -3,10 +3,21 @@ const Comment = require('../models/comment');
 
 module.exports.create = async function(req, res){
     try {
-        await Post.create({
+        let post = await Post.create({
             content: req.body.content,
             user: req.user._id
         });
+
+        if(req.xhr){
+            return res.status(200).json({
+                data: {
+                    post: post
+                },
+                message: "post created!"
+            })
+        }
+
+
         req.flash('success','post published');
         return res.redirect('back');
     } catch (err) {
@@ -25,6 +36,15 @@ module.exports.destroy = async function(req, res){
             post.remove();
 
             await Comment.deleteMany({post: req.params.id});
+
+            if(req.xhr){
+                return res.status(200).json({
+                    data: {
+                        post_id: req.params.id
+                    },
+                    message: "Post deleted successfully"
+                });
+            }
 
             req.flash('success', 'Post and associated comments deleted!');
 
