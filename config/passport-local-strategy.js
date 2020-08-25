349e912e887212f275e7c46b40a4dbe7a61751cc
Ravi Shankar Singh <ravi13eng@gmail.com>
2020-08-25 10:41:02 +0530

    file uploaded done

--- config/passport-local-strategy.js
+++ config/passport-local-strategy.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 
-const LocalStrategy = require('passport-local');
+const LocalStrategy = require('passport-local').Strategy;
 
 const User = require('../models/user');
 
